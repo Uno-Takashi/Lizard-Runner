a959c0ca82bfa2e34e6efc93b80397ac40bfa832
Takashi Uno <euno.eng@gmail.com>
2022-05-19 02:07:06 +0900

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -eax
 result_cli="test"
 result_cli=`lizard $2`
-"${result_cli//$'\n'/\\n}"
+result_cli="${result_cli//$'\n'/\\n}"
 
 echo "::group::Outputs"
 echo ::set-output name=result_cli::$result_cli
