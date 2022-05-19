3c13b5665e58469ab8580bee5bc98303fcc550d1
Takashi Uno <euno.eng@gmail.com>
2022-05-20 02:27:23 +0900

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,33 @@
 set -eax
 
 base_dir=`pwd`
-echo $base_dir
-echo $3
-echo $4
 
-cd $1
+path = $0
+cli_output_file = $1
+language = $2
+verbose = $3
+CCN = $4
+input_file = $5
+output_file = $6
+length = $7
+arguments = $8
+warnings_only = $9
+warning_msvs = $10
+ignore_warnings = $11
+exclude = $12
+working_threads = $13
+xml = $14
+html = $15
+modified = $16
+extension = $17
+sort = $15
+Threshold = $16
+whitelist = $17
+
+echo $whitelist
 
 result_cli="test"
-result_cli=`lizard $2 | tee lizard.txt`
+result_cli=`lizard | tee $cli_output_file`
 result_cli="${result_cli//$'\n'/\\n}"
 
 echo "::group::Outputs"
