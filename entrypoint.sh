918d2e1b37eb43e00ac3f9621517a4efda83f0f2
Takashi Uno <euno.eng@gmail.com>
2022-05-20 02:31:01 +0900

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,27 +3,27 @@ set -eax
 
 base_dir=`pwd`
 
-path = $0
-cli_output_file = $1
-language = $2
-verbose = $3
-CCN = $4
-input_file = $5
-output_file = $6
-length = $7
-arguments = $8
-warnings_only = $9
-warning_msvs = $10
-ignore_warnings = $11
-exclude = $12
-working_threads = $13
-xml = $14
-html = $15
-modified = $16
-extension = $17
-sort = $15
-Threshold = $16
-whitelist = $17
+path=$0
+cli_output_file=$1
+language=$2
+verbose=$3
+CCN=$4
+input_file=$5
+output_file=$6
+length=$7
+arguments=$8
+warnings_only=$9
+warning_msvs=$10
+ignore_warnings=$11
+exclude=$12
+working_threads=$13
+xml=$14
+html=$15
+modified=$16
+extension=$17
+sort=$15
+Threshold=$16
+whitelist=$17
 
 echo $whitelist
 
