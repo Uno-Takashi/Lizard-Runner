e5a87c339784cf868f5ca1050d1893dd438e7ad3
Takashi Uno <euno.eng@gmail.com>
2022-05-21 14:59:47 +0900

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,61 +2,94 @@
 set -eax
 
 echo "::group::ChangeDirectory"
-base_dir=`pwd`
 
-path=$1
+
 
 echo "::group::ValidateArguments"
-
+path=$1
 cli_output_file=$2
+cli_output_file='"$cli_output_file"'
+
 
 language=$3
-language=`echo $language | sed 's/ //g'`
+language='"$language"'
 
 verbose=$4
-verbose=`echo $verbose | sed 's/ //g'`
+verbose='"$verbose"'
 
 CCN=$5
+CCN='"$CCN"'
+
 input_file=$6
 if [ -z "$input_file" ]; then
   input_file="None"
 fi
+input_file='"$input_file"'
+
 output_file=$7
 if [ -z "$output_file" ]; then
   output_file="None"
 fi
+output_file='"$output_file"'
+
 length=$8
+length='"$length"'
+
 arguments=$9
 if [ -z "$arguments" ]; then
   arguments="None"
 fi
+arguments='"$arguments"'
+
 warnings_only=${10}
+warnings_only='"$warnings_only"'
+
 warning_msvs=${11}
+warning_msvs='"$warning_msvs"'
+
 ignore_warnings=${12}
 if [ -z "$ignore_warnings" ]; then
   ignore_warnings="None"
 fi
+ignore_warnings='"$ignore_warnings"'
+
 exclude=${13}
 if [ -z "$exclude" ]; then
   exclude="None"
 fi
+exclude='"$exclude"'
+
 working_threads=${14}
+working_threads='"$working_threads"'
+
 xml=${15}
+xml='"$xml"'
+
 html=${16}
+html='"$html"'
+
 modified=${17}
+modified='"$modified"'
 extension=${18}
 if [ -z "$extension" ]; then
   extension="None"
 fi
+extension='"$extension"'
+
 sort=${19}
 if [ -z "$sort" ]; then
   sort="None"
 fi
+sort='"$sort"'
+
 Threshold=${20}
 if [ -z "$Threshold" ]; then
   Threshold="None"
 fi
+Threshold='"$Threshold"'
+
 whitelist=${21}
+whitelist='"$whitelist"'
 
 lizard_args=`python /lib/lizard_argument_validator.py \
                     -language $language \
--- lizard_argument_validator.py
+++ lizard_argument_validator.py
@@ -3,24 +3,28 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser(description="Validate the argument of lizard")
 parser.add_argument("-language", type=str)
-parser.add_argument("-verbose", choices=["true", "false"], type=str)
+parser.add_argument("-verbose", choices=["true", "false"], type=str, default="false")
 parser.add_argument("-CCN", type=int)
-parser.add_argument("-input_file", type=str)
-parser.add_argument("-output_file", type=str)
+parser.add_argument("-input_file", type=str, default="None")
+parser.add_argument("-output_file", type=str, default="None")
 parser.add_argument("-length", type=int)
-parser.add_argument("-arguments", type=str)
-parser.add_argument("-warnings_only", choices=["true", "false"], type=str)
-parser.add_argument("-warning_msvs", choices=["true", "false"], type=str)
-parser.add_argument("-ignore_warnings", type=str)
-parser.add_argument("-exclude", type=str)
+parser.add_argument("-arguments", type=str, default="None")
+parser.add_argument(
+    "-warnings_only", choices=["true", "false"], type=str, default="false"
+)
+parser.add_argument(
+    "-warning_msvs", choices=["true", "false"], type=str, default="false"
+)
+parser.add_argument("-ignore_warnings", type=str, default="None")
+parser.add_argument("-exclude", type=str, default="None")
 parser.add_argument("-working_threads", type=int)
-parser.add_argument("-xml", choices=["true", "false"], type=str)
-parser.add_argument("-html", choices=["true", "false"], type=str)
-parser.add_argument("-modified", choices=["true", "false"], type=str)
-parser.add_argument("-extension", type=str)
-parser.add_argument("-sort", type=str)
-parser.add_argument("-Threshold", type=str)
-parser.add_argument("-whitelist", type=Path)
+parser.add_argument("-xml", choices=["true", "false"], type=str, default="false")
+parser.add_argument("-html", choices=["true", "false"], type=str, default="false")
+parser.add_argument("-modified", choices=["true", "false"], type=str, default="false")
+parser.add_argument("-extension", type=str, default="None")
+parser.add_argument("-sort", type=str, default="None")
+parser.add_argument("-Threshold", type=str, default="None")
+parser.add_argument("-whitelist", type=Path, default="None")
 
 args = parser.parse_args()
 
